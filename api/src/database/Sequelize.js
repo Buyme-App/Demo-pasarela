4afbe0ac47a9b00b781a2974af127e0fe30aac1c
JoseGarcia <josernestogarcia609@gmail.com>
2022-02-15 10:01:39 -0300

    modif

--- api/src/database/Sequelize.js
+++ api/src/database/Sequelize.js
@@ -2,10 +2,10 @@
 
 // require("dotenv").config();
 // const { Sequelize } = require("sequelize");
-// // const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+// // const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 // const sequelize = new Sequelize(
-//   `postgres://xnkskfubzlidys:aa49dd49f32d57474064ea527e3ca7551b3414a11eac71f8fe0fdf9cd600ba47@ec2-34-205-46-149.compute-1.amazonaws.com/d6v5jq7a9i0ren`,
+//     `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
 //   {
 //     // ecommerce es el nombre de la base de datos local
 //     logging: false, // set to console.log to see the raw SQL queries
--- api/src/routes/index.routes.js
+++ api/src/routes/index.routes.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const routeHome = require('./home.routes');
+const express = require("express");
 
+const routeHome = require("./home.routes");
 
-
-
+//mp
+const mp = require("./mercadopago/mpAccess.route"); //agregar para mercado pago
+const notificationOrder = require("./mercadopago/notificationOrder.route"); // agregar para recibir notificacion
 
 const routes = (server) => {
-    server.use('/home', routeHome);
-    
-
-
-}
+  server.use("/home", routeHome);
+  //mp
+  server.use("/mp", mp); //agregar para mercado pago
+  server.use("/notification", notificationOrder); // Se agrega para el url de respuesta
+};
 
 module.exports = routes; // Update
