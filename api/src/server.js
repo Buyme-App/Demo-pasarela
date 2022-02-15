57e4b2e1cee940e1e194e980001ce90914bfd71a
nicolius888 <nicola.edgardo@gmail.com>
2022-02-14 23:18:49 -0300

    force:false

--- api/src/server.js
+++ api/src/server.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 const routes = require('./routes/index.routes');
 const cors = require('cors');
 const bodyParser = require("body-parser");
-
+const {CORS_URL}
 const server = express();
 
 // Middlewares
@@ -13,7 +13,7 @@ server.use(morgan('dev'));
 server.use(cors());
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', CORS_URL);
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
