399c624bfd0c2a90da9d60dfeb7f6ae1bc95f95d
Nicolás Sammaritano <82170757+Nicolius888@users.noreply.github.com>
2022-02-17 06:22:43 -0800

    cambio el link de axios a backend v2

--- client/src/actions.js
+++ client/src/actions.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 export function sendCart(data){
 
     try {
-        // axios.post('https://demo-pasarela.herokuapp.com/mp', data);
-        axios.post('http://localhost:3001/mp', data);
+         axios.post('https://demo-pasarela-v2.herokuapp.com/mp', data);
+        //axios.post('http://localhost:3001/mp', data);
         console.log('Actions>>>>>>>', data)
     } catch (error) {
         console.log('Error>>>>>', error)
