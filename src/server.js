1c3a214c810e706b39940d197fc713ca9d8ce8c3
nicolius888 <nicola.edgardo@gmail.com>
2022-02-17 17:23:19 -0300

    add cors link

--- src/server.js
+++ src/server.js
@@ -12,14 +12,11 @@ server.use(express.json());
 server.use(morgan("dev"));
 // server.use(cors());
 server.use(
-  cors({ origin: "https://demo-pasarela.vercel.app/", credentials: true })
+  cors({ origin: "https://demo-pasarela.vercel.app", credentials: true })
 );
 
 server.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://demo-pasarela.vercel.app/"
-  );
+  res.header("Access-Control-Allow-Origin", "https://demo-pasarela.vercel.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
