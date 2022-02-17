6f3357ce2ec5927d20c9edb682fb823fa21e2d35
nicolius888 <nicola.edgardo@gmail.com>
2022-02-17 17:08:24 -0300

    add cors variable in invoking

--- src/server.js
+++ src/server.js
@@ -3,17 +3,18 @@ const morgan = require("morgan");
 const routes = require("./routes/index.routes");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const { CORS_URL } = process.env;
+// const { CORS_URL } = process.env;
 const server = express();
 
 // Middlewares
 server.use(bodyParser.json({ limit: "10mb" }));
 server.use(express.json());
 server.use(morgan("dev"));
-server.use(cors());
+// server.use(cors());
+server.use(cors({ origin: process.env.CORS_URL, credentials: true }));
 
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", CORS_URL);
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
